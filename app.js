9472fbdf9704136b3379f510bbd19085255a65ef
Denys <sagottw@gmail.com>
2021-05-28 15:12:09 +0300

    fixed js probl

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ dateOfBirth.addEventListener("focus", function() {
 dateOfBirth.addEventListener("focusout", function() {
     dateOfBirth.type = "text"
 })
+
+console.log(userName.value)
 button.addEventListener("click", function() {
-    if (userName.textContent.length == 0 && userEmail.textContent.length == 0 && userMessage.textContent.length == 0 && dateOfBirth.textContent.length == 0) {
-        alert ("Please fill all the information")
+    if (userName.length == 0 && userEmail.length == 0 && userMessage.length == 0 && dateOfBirth.length == 0 ) {
+        alert("Please fill all the information")
     } else {
-        alert ("Information sent successfully")
+        alert("Information sent successfully")
     }
 })
 
