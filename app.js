8eaff769a768c896ac94a3eee43024d5463102d5
Denys <sagottw@gmail.com>
2021-05-28 14:17:37 +0300

    Finished 1st page (no adaptive)

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+let dateOfBirth = document.querySelector(".textbox-birth");
+
+dateOfBirth.addEventListener("focus", function() {
+    dateOfBirth.type = "date"
+})
+
+dateOfBirth.addEventListener("focusout", function() {
+    dateOfBirth.type = "text"
+})
+
 new Swiper(".swiper-container", {
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
     },
     pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
+      el: ".swiper-pagination",
+      dynamicBullets: true
     },
     slidesPerView: 5,
     spaceBetween: 32,
-    loop: true,
-});
+    loop: true
+  });
