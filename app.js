d2d221a32f7d90b2372275b11d289c9500f76f3d
Denys <sagottw@gmail.com>
2021-05-28 14:45:01 +0300

    need to fix fields filling

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-let dateOfBirth = document.querySelector(".textbox-birth");
+let userName = document.querySelector(".name");
+let userEmail = document.querySelector(".email")
+let userMessage = document.querySelector(".message");
+let dateOfBirth = document.querySelector(".birth");
+let button = document.querySelector(".send")
 
 dateOfBirth.addEventListener("focus", function() {
     dateOfBirth.type = "date"
 })
-
 dateOfBirth.addEventListener("focusout", function() {
     dateOfBirth.type = "text"
+})
+button.addEventListener("click", function() {
+    if (userName.textContent.length == 0 && userEmail.textContent.length == 0 && userMessage.textContent.length == 0 && dateOfBirth.textContent.length == 0) {
+        alert ("Please fill all the information")
+    } else {
+        alert ("Information sent successfully")
+    }
 })
 
 new Swiper(".swiper-container", {
